0b7f1dfa07c90f93e330429a74b87a27405d5c6b
xuyonghui <2541128175@qq.com>
2024-09-03 10:48:11 +0800

    fix(test): 修复单元测试中的断言错误

--- eslint.config.js
+++ eslint.config.js
@@ -5,28 +5,26 @@ import tseslint from 'typescript-eslint'
 // vue文件解析器
 import vueParser from 'vue-eslint-parser'
 export default tseslint.config({
-  // tseslint.config添加了extends扁平函数，直接用。否则是eslint9.0版本是没有extends的
-  extends: [
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-    ...pluginVue.configs['flat/essential'] // vue3推荐的eslint配置
-  ],
-  languageOptions: {
-    parser: vueParser, // 使用vue解析器，这个可以识别vue文件
-    parserOptions: {
-      parser: tseslint.parser, // 在vue文件上使用ts解析器
-      sourceType: 'module'
+    // tseslint.config添加了extends扁平函数，直接用。否则是eslint9.0版本是没有extends的
+    extends: [
+        eslint.configs.recommended,
+        ...tseslint.configs.recommended,
+        ...pluginVue.configs['flat/essential'] // vue3推荐的eslint配置
+    ],
+    languageOptions: {
+        parser: vueParser, // 使用vue解析器，这个可以识别vue文件
+        parserOptions: {
+            parser: tseslint.parser, // 在vue文件上使用ts解析器
+            sourceType: 'module'
+        }
+    },
+    rules: {
+        semi: ['warn', 'never'],
+        'comma-dangle': ['error', 'never'],
+        'no-unused-vars': 2,
+        'space-before-function-paren': 0,
+        'generator-star-spacing': 'off',
+        'object-curly-spacing': 0, // 强制在大括号中使用一致的空格
+        'array-bracket-spacing': 0 // 方括号
     }
-  },
-  rules: {
-    'semi': ['warn', 'never'],
-    "comma-dangle": ["error", "never"],
-    "no-unused-vars": 2,
-    'space-before-function-paren': 0,
-    'generator-star-spacing': 'off',
-    'object-curly-spacing': 0, // 强制在大括号中使用一致的空格
-    'array-bracket-spacing': 0 ,// 方括号
-    // "@typescript-eslint/no-empty-object-type": "off",
-    // "@typescript-eslint/no-explicit-any":"off"
-  }
 })
